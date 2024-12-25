7c7fcbd5a0ff22851d3a344645992394d515c1fd
Rodyslav Sinitsyn <rodyslav.sinitsyn@raiffeisen.ua>
2024-12-25 11:42:22 +0200

    Pom changes and SpringBookApp main for UI module

--- bth-ui/src/main/bth/ui/BookTheHouseUiApp.java
+++ bth-ui/src/main/bth/ui/BookTheHouseUiApp.java
@@ -1,4 +1,12 @@
 package bth.ui;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class BookTheHouseUiApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookTheHouseUiApp.class, args);
+    }
 }
