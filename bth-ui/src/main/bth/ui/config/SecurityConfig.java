9573cc9c01735c380531df2d537f3fa76ffa8838
Rodyslav Sinitsyn <radik200058@gmail.com>
2024-12-25 21:32:51 +0200

    Version 1.2 (#2)
    * Added OAuth2 integration with Google and Github
    
    - Updated 'header' fragment with authenticated user info and login/logout buttons

---
+++ bth-ui/src/main/bth/ui/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package bth.ui.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(registry -> {
+                    registry.anyRequest().permitAll();
+                })
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                        .maximumSessions(1))
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/posts", true))
+                .logout(logout -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                        .logoutSuccessUrl("/login")
+                        .invalidateHttpSession(true)
+                        .clearAuthentication(true)
+                        .deleteCookies("JSESSIONID"))
+                .build();
+    }
+}
