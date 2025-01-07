66629632cfd46b73ec9434ee7779de0d172487da
Rodyslav Sinitsyn <rodyslav.sinitsyn@raiffeisen.ua>
2025-01-07 20:52:30 +0200

    - Enhanced Email content with an FreeMarker HTML template

--- bth-notificator/src/main/java/bth/notificator/service/EmailService.java
+++ bth-notificator/src/main/java/bth/notificator/service/EmailService.java
@@ -1,9 +1,12 @@
 package bth.notificator.service;
 
 import bth.notificator.props.MailProperties;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -23,6 +26,19 @@ public class EmailService {
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
+
+        javaMailSender.send(message);
+    }
+
+    @SneakyThrows
+    public void sendHtmlEmail(String to, String subject, String htmlContent) {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setFrom(mailProperties.getSender());
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(htmlContent, true);
 
         javaMailSender.send(message);
     }
